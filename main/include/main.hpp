8cb8b9fe3423ca4f4ecb1f6dbe3512afd66fe058
shidong_wu <epsilon5400@gmail.com>
2025-07-22 01:13:32 +0800

    增加线程管理相关文档

--- main/include/main.hpp
+++ main/include/main.hpp
@@ -5,13 +5,20 @@
 extern "C"
 {
 #endif
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "esp_log.h"
-#include "sdkconfig.h"
+
 #ifdef __cplusplus
 }
 #endif
+#include <thread>
+#include <chrono>
+#include <memory>
+#include <string>
+#include <sstream>
+#include <esp_pthread.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <esp_log.h>
 #include <iostream>
 #include "bsp_gpio.hpp"
+#include <sdkconfig.h>
 #endif // MAIN_HPP
